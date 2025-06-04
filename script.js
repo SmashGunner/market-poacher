b0916e72d793e05e2132231ada90754a1afbd911
SmashGunner <ssimamgan@gmail.com>
2025-06-05 00:03:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const toggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
